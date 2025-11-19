b59d18712f5cab7bc506eb5a26336318dbae43ee
KeerthanaNallamotu <keerthana.nallamotu@gmail.com>
2025-11-19 12:45:46 -0500

    Updated MSigDB tool to fix gene set extraction

--- src/tools/msigdb/utils.py
+++ src/tools/msigdb/utils.py
@@ -1,36 +1,34 @@
 import json
+import os
 
-def create_gene_based_annotation_file():
-    with open("../../../local_dbs/msigdb.v2025.1.Hs.json.txt", "r") as file:
+def create_gene_based_annotation_file(orig_annotation_file):
+    with open(orig_annotation_file, "r") as file:
         data = json.load(file)
 
     gene_dict = {}
 
     for gene_set_name, info in data.items():
-        genes = info["geneSymbols"]
-        new_info = {k: v for k, v in info.items() if k != "geneSymbols"}
-
+        genes = info.get("geneSymbols", [])
         collection_name = info.get("collection", "Unknown")
 
         for gene in genes:
             if gene not in gene_dict:
                 gene_dict[gene] = {}
-            if collection_name not in gene_dict[gene]:
-                gene_dict[gene][collection_name] = []
-            
-            annotation_entry = {"geneSetName": gene_set_name}
-            annotation_entry.update(new_info)
-            gene_dict[gene][collection_name].append(annotation_entry)
+            gene_dict[gene][gene_set_name] = collection_name
 
-    with open("../../../local_dbs/gene_centered_msigdb.v2025.1.Hs.json.txt", "w") as file:
+    orig_file_name = orig_annotation_file.split('/')[-1]
+    prefix = os.path.dirname(orig_annotation_file)
+
+    with open(f"{prefix}/gene_centered_{orig_file_name}", "w") as file:
         json.dump(gene_dict, file, indent=4)
 
 
 def extract_geneSetNames(gene_dict, gene, collection_substring):
     geneSetNames = set()
     if gene in gene_dict:
-        for collection, annotations in gene_dict[gene].items():
-            if collection_substring in collection:
-                for annotation in annotations:
-                    geneSetNames.add(annotation["geneSetName"])
+        for geneSetName, collection in gene_dict[gene].items():
+            if collection_substring == collection.split(":")[0]:
+                geneSetNames.add(geneSetName)
     return list(geneSetNames)
+
+
